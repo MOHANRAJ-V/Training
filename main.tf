62a26fa9b96d262abe25bde9b4a880deb72e6480
MOHANRAJ-V <67055545+MOHANRAJ-V@users.noreply.github.com>
2024-03-13 00:32:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,12 +12,12 @@ terraform {
 provider "aws" {
   access_key = "AKIAR6HSDZJV5D2MKJW4"
   secret_key = "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"
-  region  = "us-west-2"
+  region  = "eu-north-1"
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-08f7912c15ca96832"
-  instance_type = "t2.micro"
+  ami           = "ami-00381a880aa48c6c6"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "ExampleAppServerInstance"
