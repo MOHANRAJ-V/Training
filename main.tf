2395d04408c94953012dc47256065ef87542dba9
MOHANRAJ-V <67055545+MOHANRAJ-V@users.noreply.github.com>
2024-04-24 14:34:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,14 +10,14 @@ terraform {
 }
 
 provider "aws" {
-  access_key = "AKIAR6HSDZJV5D2MKJW4"
-  secret_key = "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"
+  access_key = "AKIAY03HLYWBHCAYSA03"
+  secret_key = "k6aqQpK2R38MVMtJXivLTa0LhIDQeTNGRcIjMqMf"
   region  = "eu-north-1"
 }
 
 resource "aws_instance" "jenkins-ec2" {
   ami           = "ami-00381a880aa48c6c6"
-  instance_type = "t3.micro"
+  instance_type = "t3.micro"terra form 
   key_name      = "Tesing"
   user_data    |= file(install_jenkins.sh)
 
